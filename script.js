59938efa10833840248ae2e349d4d80cc9e635f4
Olgaklopotskaya <olka_250895@mail.ru>
2023-04-11 01:31:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const sendInput = document.querySelector(".input-wrapper");
+
+
+sendInput.addEventListener("keydown", function(event) {
+    const GroceriesText = sendInput.value;
+
+    const newGroceries = document.createElement("div");
+    newGroceries.classList.add("new");
+    newGroceries.textContent = GroceriesText;
+
+
+    const groceries = document.createElement("div");
+    sendInput.before(groceries);
+
+
+    if (event.key == 'Enter') {
+        groceries.prepend(newGroceries);
+    }
+    sendInput.value = " ";
+})
+
+const newGroceries = document.querySelectorAll('div');
+
+for (let div of newGroceries) {
+    div.addEventListener('click', function() {
+        div.classList.toggle('done');
+
+    })
+}
+
+
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
